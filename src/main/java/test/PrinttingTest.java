b4663e4b0d2d7dac0cd0ac29a9762480230b786a
sabuhiiqbal <iqbal.sabuhi@gmail.com>
2020-04-23 12:33:45 -0400

    added new jenkins project

---
+++ src/main/java/test/PrinttingTest.java
@@ -0,0 +1,15 @@
+package test;
+
+public class PrinttingTest {
+	public static void main(String[] args) {
+		
+	}
+	public void hello() {
+		System.out.println("HEllo world");
+
+	}
+	public void testJenkins() {
+		System.out.println("Testing jenkins");
+	}
+
+}
